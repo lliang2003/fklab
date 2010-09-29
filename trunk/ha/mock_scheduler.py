674472fe19e3c24ef9ecf953dccf9cacb30f1d64
fankaicn <fankaicn@40378bc4-d64f-11de-9196-2db6e9c7c7ae>
2010-09-29 03:17:44 +0000

    daily submit

--- trunk/ha/mock_scheduler.py
+++ trunk/ha/mock_scheduler.py
@@ -2,7 +2,8 @@
 # @author   FAN Kai (fankai@net.pku.edu.cn), Peking University  
 # @date     Sep 24 09:17:43 PM CST 2010
 
-import random, Queue
+import random, Queue, heapq
+import time
 
 class Task:
     def __init__(self, machine_count, backup_count):
@@ -13,6 +14,10 @@ class Slot:
     def __init__(self, machine_id):
         self.machine = machine_id
         self.id = random.randint(0, 99)
+        self.free_time = 0
+
+    def __cmp__(self, other):
+        return self.free_time - other.free_time
 
 
 def find_task(tasks, machine):
@@ -29,25 +34,19 @@ def find_task(tasks, machine):
     return None
 
 
-def run():
-    nm = 200
-    nt = 800
+def run(nm, nt):
     slots = []
     tasks = []
-    squeue = Queue.Queue()
     for i in range(nt):
         tasks.append(Task(nm, 3))
     for i in range(nm*3):
         slots.append(Slot(i/3))
     random.shuffle(slots)
-    for slot in slots:
-        #print slot.machine
-        squeue.put(slot)
 
     nlocal, nremote = 0, 0
     #for i in range(nt):
     while True:
-        slot = squeue.get()
+        slot = heapq.heappop(slots)
         task = find_task(tasks, slot.machine)
         if task == None: break
         #print task, task.done
@@ -56,12 +55,22 @@ def run():
             nlocal += 1
         else:
             nremote += 1
-        squeue.put(slot)
+        slot.free_time = time.time() +  random.normalvariate(10, 5)
+        heapq.heappush(slots, slot)
+        #print slot.id, slot.free_time-time.time()
 
-    print nlocal, nremote
+    #print nlocal, nremote
+    return nremote
 
 
-run()
+for nm in [200, 400, 800]:
+    for nt in range(nm/2, nm*10, nm/2):
+        nremote = 0
+        for i in range(5):
+            nremote += run(nm, nt)
+        nremote /= 5
+        #print nm, nt, "\t", nremote, nt-nremote
+        print nm, nt, "\t", nremote*100.0/nt
 
 
 
