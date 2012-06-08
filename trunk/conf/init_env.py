32c5286435a11354caa00a7dbc14b5545711032b
fankaicn <fankaicn@40378bc4-d64f-11de-9196-2db6e9c7c7ae>
2012-06-08 13:39:49 +0000

    add devdfc

--- trunk/conf/init_env.py
+++ trunk/conf/init_env.py
@@ -14,9 +14,9 @@ def create_links():
     echo_exe('ln -sf %s/_emacs ~/.emacs' % conf_path)
     echo_exe('ln -sf %s/vimrc ~/.vimrc' % conf_path)
     echo_exe('ln -sf %s/vim ~/.vim' % conf_path)
-    echo_exe('ln -sf %s/bashrc.root /root/.bashrc' % conf_path)
-    echo_exe('ln -sf %s/vimrc /root/.vimrc' % conf_path)
-    echo_exe('ln -sf %s/vim /root/.vim' % conf_path)
+#    echo_exe('ln -sf %s/bashrc.root /root/.bashrc' % conf_path)
+#    echo_exe('ln -sf %s/vimrc /root/.vimrc' % conf_path)
+#    echo_exe('ln -sf %s/vim /root/.vim' % conf_path)
     sys.stdout.flush()
 
 def check_links():
@@ -25,15 +25,15 @@ def check_links():
     os.system('ls -l --color=auto ~/.emacs')
     os.system('ls -l --color=auto ~/.vimrc')
     os.system('ls -l --color=auto ~/.vim')
-    os.system('ls -l --color=auto /root/.bashrc')
-    os.system('ls -l --color=auto /root/.vimrc')
-    os.system('ls -l --color=auto /root/.vim')
+#    os.system('ls -l --color=auto /root/.bashrc')
+    #os.system('ls -l --color=auto /root/.vimrc')
+    #os.system('ls -l --color=auto /root/.vim')
 
 
 if __name__ == '__main__':
-    if not os.getuid() == 0:
-        print 'Require root privilege.'
-        sys.exit(1)
+#    if not os.getuid() == 0:
+#        print 'Require root privilege.'
+#        sys.exit(1)
     print 'Initializing environment ...'
     create_links()
     check_links()
